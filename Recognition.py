c8932f3c3d4971aaf608f9a7ad06dc2e43355099
SkartenV <vicente.pacheco@sansano.usm.cl>
2020-06-08 17:04:39 -0400

    Probabilidad de reconocimiento

--- Recognition.py
+++ Recognition.py
@@ -3,7 +3,13 @@ import os.path as path
 
 def detect_text(photo, bucket):
 
-    client=boto3.client('rekognition')
+    client = boto3.client(
+        'rekognition',
+        aws_access_key_id = "ASIAZD5Z3NSCMLQHS26T",
+        aws_secret_access_key = "F8AjmZwdUXtpQ3cO9sjspjSwbNdOGxcL5dSBSvwL",
+        aws_session_token = "FwoGZXIvYXdzEJ7//////////wEaDKm85UX0Y7ZwCNxclCLNAf8uOWoe96SOIsECoGBqcSOUtiY91bwXdn0uJMzG2HFxGC7pNfm9xYRfuKxAeW1+PQTmQXooM6Yo8leho/TAIzjoMHuCLNO/iflin4RTPPYE8cV143fT+gwAPD7qmfHqNzrUXous+8TLWnXarTBX/z2+fNxjJb+pCI57FC//pyKjXF3jjVzwEgGL4ig51yo/En74UVfeg2Ep+pDCBlrPi5PlVU4vCK74PHKgy7F1V3JHfUBbDgC1RtxNUA0GROuf7DW4R5yJx05SV8TTp6AosLX69gUyLcOJQIV6Nl9YVo2n/woBDcJaZYzwya0m7lMY/pwpIaFtXJIjNVUhW8fSnbpzSw=="
+    )
+
     Arreglo_Texto = []
 
     response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
@@ -11,15 +17,20 @@ def detect_text(photo, bucket):
     textDetections=response['TextDetections']
     print ('Detected text\n----------')
     for text in textDetections:
-            print ('Detected text:' + text['DetectedText'])
-            if(text['Type'] == "WORD"):
-                Arreglo_Texto.append(text['DetectedText'])
-            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-            print ('Id: {}'.format(text['Id']))
-            if 'ParentId' in text:
-                print ('Parent Id: {}'.format(text['ParentId']))
-            print ('Type:' + text['Type'])
-            print()
+
+        print ('Detected text:' + text['DetectedText'])
+        print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+        print ('Id: {}'.format(text['Id']))
+        if 'ParentId' in text:
+            print ('Parent Id: {}'.format(text['ParentId']))
+        print ('Type:' + text['Type'])
+        print()
+
+        if(text['Type'] == "WORD" and float("{:.2f}".format(text['Confidence'])) >= 97.00):
+            Arreglo_Texto.append(text['DetectedText'])
+        elif(text['Type'] == "WORD" and float("{:.2f}".format(text['Confidence'])) < 97.00):
+            return False
+
     return Arreglo_Texto
 
 def main():
@@ -38,6 +49,12 @@ def main():
 
     TextoDeControl = detect_text(ImagenDeControl,bucket)
     TextoDePrueba = detect_text(ImagenDePrueba,bucket)
+
+    if(TextoDePrueba == False):
+        Archivo_logs.write("Valor retornado: False. Una o mas palabras no superaron la probabilidad de reconocimiento (97%).")
+        Archivo_logs.close()
+        print("False")
+        return False
     
     for i in range(len(TextoDeControl)):
         TextoDeControl[i] = TextoDeControl[i].lower()
@@ -45,8 +62,8 @@ def main():
     for i in range(len(TextoDePrueba)):
         TextoDePrueba[i] = TextoDePrueba[i].lower()
 
-    Archivo_logs.write("Nombre imagen: \"" + ImagenDeControl + "\" - Palabras encontradas: " + str(TextoDeControl) + "\n")
-    Archivo_logs.write("Nombre imagen: \"" + ImagenDePrueba + "\" - Palabras encontradas: " + str(TextoDePrueba) + "\n\n")
+    Archivo_logs.write("Nombre imagen de control: \"" + ImagenDeControl + "\" - Palabras encontradas: " + str(TextoDeControl) + "\n")
+    Archivo_logs.write("Nombre imagen de prueba: \"" + ImagenDePrueba + "\" - Palabras encontradas: " + str(TextoDePrueba) + "\n\n")
 
     if(set(TextoDePrueba).issubset(set(TextoDeControl))):
         Archivo_logs.write("Valor retornado: True. Se encontraron las palabras de la imagen de prueba dentro de la imagen de control.")

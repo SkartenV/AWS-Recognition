94e3bcca3c4ccbb795e2da998ff81e0bad7f9e40
SkartenV <vicente.pacheco@sansano.usm.cl>
2020-06-07 20:25:26 -0400

    Logs

--- Recognition.py
+++ Recognition.py
@@ -6,8 +6,7 @@ def detect_text(photo, bucket):
     client=boto3.client('rekognition')
     Arreglo_Texto = []
 
-    with open(photo, 'rb') as image:
-        response=client.detect_text(Image={'Bytes': image.read()})
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
                         
     textDetections=response['TextDetections']
     print ('Detected text\n----------')
@@ -25,11 +24,12 @@ def detect_text(photo, bucket):
 
 def main():
 
-    bucket='bucket'
+    Archivo_logs = open("logs.txt","w")
+    bucket='bucketskartenv1'
     ImagenDeControl='Control.png'
 
     while(True):
-        print("Ingrese el nombre de la imagen que quiere comparar con la Imagen de Control (alguna imagen dentro de la misma carpeta):")
+        print("Ingrese el nombre de la imagen que quiere comparar con la Imagen de Control (alguna imagen dentro del bucket):")
         ImagenDePrueba = input()
         if(path.exists(ImagenDePrueba)):
             break
@@ -45,11 +45,20 @@ def main():
     for i in range(len(TextoDePrueba)):
         TextoDePrueba[i] = TextoDePrueba[i].lower()
 
-    print(TextoDeControl)
-    print(TextoDePrueba)
+    Archivo_logs.write("Nombre imagen: \"" + ImagenDeControl + "\" - Palabras encontradas: " + str(TextoDeControl) + "\n")
+    Archivo_logs.write("Nombre imagen: \"" + ImagenDePrueba + "\" - Palabras encontradas: " + str(TextoDePrueba) + "\n\n")
 
-    if(TextoDeControl == TextoDePrueba):
+    if(set(TextoDePrueba).issubset(set(TextoDeControl))):
+        Archivo_logs.write("Valor retornado: True. Se encontraron las palabras de la imagen de prueba dentro de la imagen de control.")
+        Archivo_logs.close()
+        print("True")
         return True
+    
+    else:
+        Archivo_logs.write("Valor retornado: False. No se encontraron las palabras de la imagen de prueba dentro de la imagen de control.")
+        Archivo_logs.close()
+        print("False")
+        return False
 
 
 if __name__ == "__main__":

cddb8f34f85690ef8d95c8d83fcb5b03f43c78dd
SkartenV <vicente.pacheco@sansano.usm.cl>
2020-06-09 00:18:14 -0400

    Menu

--- Recognition.py
+++ Recognition.py
@@ -3,12 +3,7 @@ import os.path as path
 
 def detect_text(photo, bucket):
 
-    client = boto3.client(
-        'rekognition',
-        aws_access_key_id = "ASIAZD5Z3NSCMLQHS26T",
-        aws_secret_access_key = "F8AjmZwdUXtpQ3cO9sjspjSwbNdOGxcL5dSBSvwL",
-        aws_session_token = "FwoGZXIvYXdzEJ7//////////wEaDKm85UX0Y7ZwCNxclCLNAf8uOWoe96SOIsECoGBqcSOUtiY91bwXdn0uJMzG2HFxGC7pNfm9xYRfuKxAeW1+PQTmQXooM6Yo8leho/TAIzjoMHuCLNO/iflin4RTPPYE8cV143fT+gwAPD7qmfHqNzrUXous+8TLWnXarTBX/z2+fNxjJb+pCI57FC//pyKjXF3jjVzwEgGL4ig51yo/En74UVfeg2Ep+pDCBlrPi5PlVU4vCK74PHKgy7F1V3JHfUBbDgC1RtxNUA0GROuf7DW4R5yJx05SV8TTp6AosLX69gUyLcOJQIV6Nl9YVo2n/woBDcJaZYzwya0m7lMY/pwpIaFtXJIjNVUhW8fSnbpzSw=="
-    )
+    client = boto3.client('rekognition')
 
     Arreglo_Texto = []
 
@@ -38,45 +33,63 @@ def main():
     Archivo_logs = open("logs.txt","w")
     bucket='bucketskartenv1'
     ImagenDeControl='Control.png'
+    NumCasoPrueba = 1
 
     while(True):
-        print("Ingrese el nombre de la imagen que quiere comparar con la Imagen de Control (alguna imagen dentro del bucket):")
-        ImagenDePrueba = input()
-        if(path.exists(ImagenDePrueba)):
+
+        print("\n -------------- Menu --------------")
+        print("| 1 - Ingresar una imagen de prueba |")
+        print("| 2 - Salir                         |")
+        print(" ----------------------------------\n")
+
+        print("Ingrese una opcion: ")
+        opcion = input()
+
+        if(opcion == '2'):
             break
+
+        if(opcion == '1'):
+
+            while(True):
+                print("Ingrese el nombre de la imagen que quiere comparar con la Imagen de Control (alguna imagen dentro del bucket):")
+                ImagenDePrueba = input()
+                if(path.exists(ImagenDePrueba)):
+                    break
+                else:
+                    print("No existe ese archivo, ingrese nuevamente.")
+
+            TextoDeControl = detect_text(ImagenDeControl,bucket)
+            TextoDePrueba = detect_text(ImagenDePrueba,bucket)
+
+            Archivo_logs.write("Caso de prueba " + str(NumCasoPrueba) + ":\n\n")
+            NumCasoPrueba += 1
+
+            if(TextoDePrueba == False):
+                Archivo_logs.write("Valor retornado: False. Una o mas palabras no superaron la probabilidad de reconocimiento (97%).\n")
+                Archivo_logs.write("\n---------------------------------------------------------------------------------------------------------------\n\n")
+            
+            else:
+                for i in range(len(TextoDeControl)):
+                    TextoDeControl[i] = TextoDeControl[i].lower()
+
+                for i in range(len(TextoDePrueba)):
+                    TextoDePrueba[i] = TextoDePrueba[i].lower()
+
+                Archivo_logs.write("Nombre imagen de control: \"" + ImagenDeControl + "\" - Palabras encontradas: " + str(TextoDeControl) + "\n")
+                Archivo_logs.write("Nombre imagen de prueba: \"" + ImagenDePrueba + "\" - Palabras encontradas: " + str(TextoDePrueba) + "\n\n")
+
+                if(set(TextoDeControl).issubset(set(TextoDePrueba))):
+                    Archivo_logs.write("Valor retornado: True. Se encontraron las palabras de la imagen de control dentro de la imagen de prueba.")
+                    Archivo_logs.write("\n\n---------------------------------------------------------------------------------------------------------------\n\n")
+                
+                else:
+                    Archivo_logs.write("Valor retornado: False. No se encontraron las palabras de la imagen de control dentro de la imagen de prueba.")
+                    Archivo_logs.write("\n\n---------------------------------------------------------------------------------------------------------------\n\n")
+
         else:
-            print("No existe ese archivo, ingrese nuevamente.")
+            print("\nOpcion invalida, seleccione nuevamente")
 
-    TextoDeControl = detect_text(ImagenDeControl,bucket)
-    TextoDePrueba = detect_text(ImagenDePrueba,bucket)
-
-    if(TextoDePrueba == False):
-        Archivo_logs.write("Valor retornado: False. Una o mas palabras no superaron la probabilidad de reconocimiento (97%).")
-        Archivo_logs.close()
-        print("False")
-        return False
-    
-    for i in range(len(TextoDeControl)):
-        TextoDeControl[i] = TextoDeControl[i].lower()
-
-    for i in range(len(TextoDePrueba)):
-        TextoDePrueba[i] = TextoDePrueba[i].lower()
-
-    Archivo_logs.write("Nombre imagen de control: \"" + ImagenDeControl + "\" - Palabras encontradas: " + str(TextoDeControl) + "\n")
-    Archivo_logs.write("Nombre imagen de prueba: \"" + ImagenDePrueba + "\" - Palabras encontradas: " + str(TextoDePrueba) + "\n\n")
-
-    if(set(TextoDePrueba).issubset(set(TextoDeControl))):
-        Archivo_logs.write("Valor retornado: True. Se encontraron las palabras de la imagen de prueba dentro de la imagen de control.")
-        Archivo_logs.close()
-        print("True")
-        return True
-    
-    else:
-        Archivo_logs.write("Valor retornado: False. No se encontraron las palabras de la imagen de prueba dentro de la imagen de control.")
-        Archivo_logs.close()
-        print("False")
-        return False
-
+    Archivo_logs.close()
 
 if __name__ == "__main__":
     main()
